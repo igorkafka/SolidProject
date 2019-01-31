b5c152fc409576a35176a1a37a8a726e6ffff20c
igor_kefka <igor_memories@hotmail.com>
2019-01-31 15:42:23 -0300

    DIP

---
+++ S.O.L.I.D/ISP/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public class Document
+    {
+        
+    }
+
+    interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    interface IMultiFunctionDevice: IPrinter, IScanner
+    {
+        
+    }
+
+    class MultiFunctionMachine: IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+           scanner.Scan(d);
+        }
+    }
+    class PhotoCopier: IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class MultiFunctionPrinter: IMachine
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class OldFanshioned:IMachine
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
